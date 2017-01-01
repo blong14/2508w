28a285c143126edfa7f821e302d91c2a0d52592d
Ben Long <14benj@gmail.com>
2017-01-01 15:06:30 -0500

    Resolving angular dependency issue

--- app/scripts/modules/application/app.js
+++ app/scripts/modules/application/app.js
@@ -1,29 +1,24 @@
 'use strict';
 
-angular
-  .module('2508wApp', [
-    'ngAnimate',
-    'ngRoute',
-    'Common'
-  ])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/blvd/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/blvd/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/blvd/apps', {
-        templateUrl: 'views/application.html',
-        controller: 'AppsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/blvd/'
-      });
+var app = angular.module('2508wApp', ['ngAnimate','ngRoute','Common'])
+            .config(['$routeProvider','$locationProvider',function ($routeProvider, $locationProvider) {
+              $routeProvider
+                .when('/blvd/', {
+                  templateUrl: 'views/main.html',
+                  controller: 'MainCtrl'
+                })
+                .when('/blvd/about', {
+                  templateUrl: 'views/about.html',
+                  controller: 'AboutCtrl'
+                })
+                .when('/blvd/apps', {
+                  templateUrl: 'views/application.html',
+                  controller: 'AppsCtrl'
+                })
+                .otherwise({
+                  redirectTo: '/blvd/'
+                });
 
-      // use the HTML5 History API
-      $locationProvider.html5Mode(true);
-  });
+                // use the HTML5 History API
+                $locationProvider.html5Mode(true);
+            }]);
